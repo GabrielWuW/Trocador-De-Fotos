9094e419a1e4096cedbbc7094374a2b7c5b13c75
unknown <gabriel4321uo@gmail.com>
2025-08-10 23:06:06 -0300

    Modo automatico adicionado agora só preciso mudar a troca das imagens para usar uma variavel no css

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const btnPesquisar = document.getElementById('btnPesquisar');
+const btnAutomatico = document.getElementById('btnAutomatico');
+
 const img = document.getElementById('imagem');
-const campoPesquisa = document.getElementById('campoPesquisa').value;
+let automatico = false;
 
 const extensoes = ['jpg', 'jpeg', 'webp'];
 
@@ -16,16 +18,56 @@ function tentaMudarImagem(nomeArquivo, index) {
     let extensaoAtual = extensoes[index];
     let url = `img/${nomeArquivo}.${extensaoAtual}`;
 
-    img.onerror = function() {
+    img.onerror = function () {
         tentaMudarImagem(nomeArquivo, index + 1);
     }
-    
+
     img.src = url;
 }
 
 function mudarImagem() {
     let nomeArquivo = document.getElementById('campoPesquisa').value;
     tentaMudarImagem(nomeArquivo, 0);
+    automatico = false;
+    btnAutomatico.innerText = 'Automático';
 }
 
 btnPesquisar.addEventListener('click', mudarImagem);
+
+const imgs = [
+    'img1',
+    'img2',
+    'img3',
+    'img4',
+    'img5',
+];
+
+function mudarImagemAutomatico(index) {
+    if (!automatico) {
+        return;
+    }
+
+    if (index >= imgs.length) {
+        index = 0;
+    }
+
+    tentaMudarImagem(imgs[index], 0);
+
+    setTimeout(() => {
+        mudarImagemAutomatico(index + 1);
+    }, 2000);
+}
+
+btnAutomatico.addEventListener('click', () => {
+    if (!automatico) {
+        mudarImagemAutomatico(0);
+        btnAutomatico.innerText = 'Parar';
+        automatico = true;
+    } else {
+        img.src = 'img/Placeholder.svg';
+        btnAutomatico.innerText = 'Automático';
+        automatico = false;
+    }
+
+    mudarImagemAutomatico(0);
+});
